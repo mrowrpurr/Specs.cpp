a75e049d11adad28f1d8516aefda904c5028024d
Mrowr Purr <mrowr.purr@gmail.com>
2023-02-11 14:30:17 -0800

    Let's use #define test(*) if(*)

--- Example/SweetDSL.h
+++ Example/SweetDSL.h
@@ -1,21 +1,8 @@
 #include <Spec/Internal/SpecStorage.h>
 
-//! Lazy way to run arbitrary function on startup by providing a function.
-struct FunctionRunner {
-    FunctionRunner(std::function<void()> fn) { fn(); }
-};
-
-// Helper macros
-#define __SPEC_MACRO_CONCAT_(x, y) x##y
-#define __SPEC_MACRO_CONCAT(x, y) __SPEC_MACRO_CONCAT_(x, y)
-
-// Create a "FunctionRunner" which takes a std::function which is run immediately at runtime
-#define __SPEC_MACRO_FUNCTIONRUNNER(count) \
-    FunctionRunner __SPEC_MACRO_CONCAT(__specGroupDefinition, count)
-
-// Setup for a callback function to be registered
-#define __SPEC_MACRO_CALLBACKFN_NAME(count) \
-    __SPEC_MACRO_CONCAT(__skyrimScriptingPluginCallback, count)
+void test_fn(const std::string& description, std::function<void()> body) {
+    //
+}
 
 void TestFunction(const std::string& description, std::function<void()> body) {
     std::cout << std::format("You registered a test function: {}", description) << std::endl;
@@ -23,18 +10,86 @@ void TestFunction(const std::string& description, std::function<void()> body) {
     // body();
 }
 
+void TestGroup(const std::string& description, std::function<void()> body) {
+    std::cout << std::format("You registered a test group: {}", description) << std::endl;
+    std::cout << "LET US NOW EVALUATE IT TO GET THE CHILD TESTS" << std::endl;
+    std::cout << "Calling the body..." << std::endl;
+    body();
+}
+
+///////////////////////////////////////////////////////////////
+
+/**
+ * Basic helpers
+ */
+
+// Helper macros
+#define __SPEC_MACRO_CONCAT_(x, y) x##y
+#define __SPEC_MACRO_CONCAT(x, y) __SPEC_MACRO_CONCAT_(x, y)
+
+/**
+ * "Function runner"
+ */
+
+//! Lazy way to run arbitrary function on startup by providing a function.
+struct FunctionRunner {
+    FunctionRunner(std::function<void()> fn) { fn(); }
+};
+
+// Create a "FunctionRunner" which takes a std::function which is run immediately at runtime
+#define __SPEC_MACRO_FUNCTIONRUNNER(count) \
+    FunctionRunner __SPEC_MACRO_CONCAT(__specGroupDefinition, count)
+
+/**
+ * _TEST outer MACRO STUFF
+ */
+
+#define __SPEC_MACRO_CALLBACKFN_NAME(count) __SPEC_MACRO_CONCAT(__spec_test_Callback, count)
 #define __SPEC_MACRO_REGISTER_AND_DEFINE_CALLBACK(description, count)               \
     inline void __SPEC_MACRO_CALLBACKFN_NAME(count)();                              \
     __SPEC_MACRO_FUNCTIONRUNNER(count){[]() {                                       \
         TestFunction(description, []() { __SPEC_MACRO_CALLBACKFN_NAME(count)(); }); \
     }};                                                                             \
     inline void __SPEC_MACRO_CALLBACKFN_NAME(count)
-
-#define test(description)                                               \
+#define _test(description)                                              \
     __SPEC_MACRO_REGISTER_AND_DEFINE_CALLBACK(description, __COUNTER__) \
     ()
 
-test("the macro test!") { std::cout << "THE MACRO DEFINED TEST RAN!" << std::endl; }
+/**
+ * _DESCRIBE outer MACRO STUFF
+ */
+
+#define __SPEC_MACRO_desribeFN_NAME(count) __SPEC_MACRO_CONCAT(__spec_test_Callback, count)
+#define __SPEC_MACRO_REGISTER_AND_DEFINE_desribe(description, count)                     \
+    inline void __SPEC_MACRO_desribeFN_NAME(count)();                                    \
+    __SPEC_MACRO_FUNCTIONRUNNER(count                                                    \
+    ){[]() { TestGroup(description, []() { __SPEC_MACRO_desribeFN_NAME(count)(); }); }}; \
+    inline void __SPEC_MACRO_desribeFN_NAME(count)
+#define _describe(description) __SPEC_MACRO_REGISTER_AND_DEFINE_desribe(description, __COUNTER__)()
+
+/** test() inner macro */
+
+#define __SPEC_MACRO_innerTest_FN_NAME(count) __SPEC_MACRO_CONCAT(__spec_test_Callback, count)
+#define __SPEC_MACRO_REGISTER_AND_DEFINE_innerTest_(description, count)                     \
+    inline void __SPEC_MACRO_innerTest_FN_NAME(count)();                                    \
+    __SPEC_MACRO_FUNCTIONRUNNER(count                                                       \
+    ){[]() { TestGroup(description, []() { __SPEC_MACRO_innerTest_FN_NAME(count)(); }); }}; \
+    inline void __SPEC_MACRO_innerTest_FN_NAME(count)
+#define test(description) __SPEC_MACRO_REGISTER_AND_DEFINE_innerTest_(description, __COUNTER__)()
+
+/** describe() inner macro */
+
+/** EXAMPLE: */
+
+#define test(description) if (description)
+
+_describe("this is a describe block") {
+    std::cout << "THIS BODY RUNS~!" << std::endl;
+
+    test("something") {}
+}
+
+_test("the macro test!") { std::cout << "THE MACRO DEFINED TEST RAN!" << std::endl; }
 
 int main() {
     TestFunction("Defined in main()", []() {
