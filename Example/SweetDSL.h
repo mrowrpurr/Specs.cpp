697418ad8534047f49fc2c476b10c585dfac0a66
Mrowr Purr <mrowr.purr@gmail.com>
2023-02-11 13:51:09 -0800

    Cleanup

--- Example/SweetDSL.h
+++ Example/SweetDSL.h
@@ -13,36 +13,15 @@ struct FunctionRunner {
 #define __SPEC_MACRO_FUNCTIONRUNNER(count) \
     FunctionRunner __SPEC_MACRO_CONCAT(__specGroupDefinition, count)
 
-// // Setup for a callback function to be registered
+// Setup for a callback function to be registered
 #define __SPEC_MACRO_CALLBACKFN_NAME(count) \
     __SPEC_MACRO_CONCAT(__skyrimScriptingPluginCallback, count)
 
-// #define OnSystemEvent(systemEvent)                                                   \
-//     __SKYRIM_SCRIPTING_PLUGIN_REGISTER_AND_DEFINE_CALLBACK(systemEvent, __COUNTER__) \
-//     ()
-
-// Ok, let's make the test macro!
-// #define test(__VA_ARGS__)
-
-// inline void __specMacro_TestFunction1()
-
-// AnyClass varName{[](){ Spec::DSL::test("something", [&](){}); }};
-
-// Spec::DSL::test("something", [&](){});
-
-// FunctionRunner runner{[]() { std::cout << "Hello from the runner!"; }};
-
 void TestFunction(const std::string& description, std::function<void()> body) {
     std::cout << std::format("You registered a test function: {}", description) << std::endl;
-    std::cout << "Calling the body..." << std::endl;
-    body();
+    // std::cout << "Calling the body..." << std::endl;
+    // body();
 }
-
-// #define test(descripton) __SPEC_MACRO_FUNCTIONRUNNER(count){[]() {                                    \
-//     TestFunction([]() { \
-//         __SPEC_MACRO_CALLBACKFN_NAME(count)();                               \
-//     });                                                                      \
-// }};
 
 #define __SPEC_MACRO_REGISTER_AND_DEFINE_CALLBACK(description, count)               \
     inline void __SPEC_MACRO_CALLBACKFN_NAME(count)();                              \
@@ -64,16 +43,3 @@ int main() {
     //
     auto& storage = Spec::Internal::SpecStorage::GetSingleton();
 }
-
-// void __RandomMethod1() {
-//     ...
-// }
-// ImmediateFunction [](){ ... ^ UnevaluatedTestBlocks }
-
-// Specs {
-// }
-// // ^ add this block to UnevaluatedSpecsBlocks
-
-// describe("...") {
-// }
-// ^ add this block to UnevaluatedDescribeBlocks
