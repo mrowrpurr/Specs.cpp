030d359907e243957871b6fa3ec508e52ee95ca5
Mrowr Purr <mrowr.purr@gmail.com>
2023-02-12 13:05:43 -0800

    Remove first proof of concept

--- include_original/Spec.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-// #include "Spec/BDD.h"
--- include_original/Spec/BDD.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Macros and everything needed for the main BDD DSL, including:
-// - describe()
-// - test() <--- alias: it()
-// - xtest() and xdescribe()
