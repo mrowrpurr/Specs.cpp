e27b05d12f87a5b01b1a6bae02b6ad7f64c54664
Mrowr Purr <mrowr.purr@gmail.com>
2023-02-15 16:31:52 -0800

    Add colors to OneLineReporter

--- include/Spec/Types/Reporters/OneLineReporter.h
+++ include/Spec/Types/Reporters/OneLineReporter.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Windows.h>
+
 #include <iostream>
 
 #include "Spec/Types/ISpecReporter.h"
@@ -7,6 +9,23 @@
 namespace Spec::Types::Reporters {
 
     class OneLineReporter : public ISpecReporter {
+        void PrintPassed(const std::string& testDescription) {
+            HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleTextAttribute(console, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+            std::cout << "PASS: " << testDescription << std::endl;
+            SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        }
+        void PrintFailed(const std::string& testDescription, const std::string& failureMessage) {
+            HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_INTENSITY);
+            if (!failureMessage.empty()) {
+                std::cout << "FAIL: " << testDescription << " (" << failureMessage << ")" << std::endl;
+            } else {
+                std::cout << "FAIL: " << testDescription << std::endl;
+            }
+            SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        }
+
     public:
         void BeginTest(SpecTest&) override {}
         void EndTest(SpecTest& test, bool passed, const std::string& failureMessage) override {
@@ -16,12 +35,10 @@ namespace Spec::Types::Reporters {
                 testDescription = group->GetDescription() + " " + testDescription;
                 group           = group->GetParent();
             }
-            if (!failureMessage.empty()) {
-                std::cout << (passed ? "PASS" : "FAIL") << ": " << testDescription << " (" << failureMessage << ")"
-                          << std::endl;
-            } else {
-                std::cout << (passed ? "PASS" : "FAIL") << ": " << testDescription << std::endl;
-            }
+            if (passed)
+                PrintPassed(testDescription);
+            else
+                PrintFailed(testDescription, failureMessage);
         }
         void BeginGroup(SpecGroup& group) override {}
         void EndGroup(SpecGroup&) override {}
