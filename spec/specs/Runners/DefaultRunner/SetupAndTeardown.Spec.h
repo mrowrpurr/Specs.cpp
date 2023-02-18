d0a93c3d7acb8f0e02d844b1e12a74e5623677ca
Mrowr Purr <mrowr.purr@gmail.com>
2023-02-17 19:20:41 -0800

    Tests for setup and teardown

--- spec/specs/Runners/DefaultRunner/SetupAndTeardown.Spec.h
+++ spec/specs/Runners/DefaultRunner/SetupAndTeardown.Spec.h
@@ -2,10 +2,43 @@
 
 TestGroup("Setup and Teardown");
 
-Setup { Print("This is the setup!"); }
-Setup { Print("This is another setup!"); }
+namespace {
+    std::string setupText;
+    std::string teardownText;
+    std::string mixedText;
+    std::string nestedText;
+}
 
-Teardown { Print("This is the teardown!"); }
+Describe("Setup runs before each test") {
+    setup([&]() { setupText += "setup "; });
+    it("test 1", [&]() { AssertThat(setupText, Equals("setup ")); });
+    it("test 2", [&]() { AssertThat(setupText, Equals("setup setup ")); });
+}
 
-Test("Something!") { Print("This is something!"); }
-Test("Something else!") { Print("This is something else!"); }
+Describe("Teardown runs after each test") {
+    teardown([&]() { teardownText += "teardown "; });
+    it("test 1", [&]() { AssertThat(teardownText, Equals("")); });
+    it("test 2", [&]() { AssertThat(teardownText, Equals("teardown ")); });
+}
+
+Describe("Setup and Teardown can be mixed") {
+    setup([&]() { mixedText += "setup "; });
+    teardown([&]() { mixedText += "teardown "; });
+    it("test 1", [&]() { AssertThat(mixedText, Equals("setup ")); });
+    it("test 2", [&]() { AssertThat(mixedText, Equals("setup teardown setup ")); });
+}
+
+Describe("Setup and Teardown can be nested") {
+    setup([&]() { nestedText += "setup "; });
+    teardown([&]() { nestedText += "teardown "; });
+    it("test 1", [&]() { AssertThat(nestedText, Equals("setup ")); });
+    it("test 2", [&]() { AssertThat(nestedText, Equals("setup teardown setup ")); });
+    describe("nested describe", [&]() {
+        setup([&]() { nestedText += "innerSetup "; });
+        teardown([&]() { nestedText += "innerTeardown "; });
+        it("test 3", [&]() { AssertThat(nestedText, Equals("setup teardown setup teardown innerSetup ")); });
+        it("test 4", [&]() {
+            AssertThat(nestedText, Equals("setup teardown setup teardown innerSetup innerTeardown innerSetup "));
+        });
+    });
+}
