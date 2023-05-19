b4813066d876890567f38628cdac682cb5ad906c
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-19 15:18:06 -0700

    [snowhouse] Support *nix file path lookup (and format changes)

--- Examples/CLI/xmake.lua
+++ Examples/CLI/xmake.lua
@@ -1,7 +1,8 @@
-add_requires("vcpkg::snowhouse")
+add_requires("vcpkg::snowhouse", "fmt")
 
 target("Specs - Example")
     set_kind("binary")
     add_deps("Specs")
     add_files("CLI.Stuff.cpp")
     add_packages("vcpkg::snowhouse", "fmt")
+    add_defines("STRING_FORMAT_USE_FMT")
--- Specs/include/Specs/ExceptionHandlers/SnowhouseExceptionHandler.h
+++ Specs/include/Specs/ExceptionHandlers/SnowhouseExceptionHandler.h
@@ -6,6 +6,7 @@
 #include <exception>
 #include <filesystem>
 #include <fstream>
+#include <optional>
 #include <sstream>
 
 #include "../ISpecExceptionHandler.h"
@@ -15,10 +16,29 @@ namespace Specs::ExceptionHandlers {
 
     //! Exception handler for assertions provided by the `Snowhouse` library.
     class SnowhouseExceptionHandler : public ISpecExceptionHandler {
-        std::string ReadLineFromFile(const std::string& filename, unsigned int linenumber) {
-            if (!std::filesystem::exists(filename)) return "";
+        std::optional<std::filesystem::path> WalkUpToFindFilePath(const std::string& path) {
+            // Given the provided path,
+            // return it if it exists.
+            //
+            // Else, walk up parent folders and return the first one that exists.
+            if (std::filesystem::exists(path)) return path;
 
-            std::ifstream file(filename);
+            std::filesystem::path currentPath = std::filesystem::current_path();
+            if (std::filesystem::exists(currentPath / path)) return currentPath / path;
+
+            while (currentPath.has_parent_path()) {
+                currentPath = currentPath.parent_path();
+                if (std::filesystem::exists(currentPath / path)) return currentPath / path;
+            }
+
+            return std::nullopt;
+        }
+
+        std::string ReadLineFromFile(const std::string& filename, unsigned int linenumber) {
+            auto path = WalkUpToFindFilePath(filename);
+            if (!path.has_value()) return "";
+
+            std::ifstream file(path.value());
             if (!file.is_open()) return "";
 
             std::string  line;
@@ -26,6 +46,10 @@ namespace Specs::ExceptionHandlers {
             while (std::getline(file, line)) {
                 currentLineNumber++;
                 if (currentLineNumber == linenumber) {
+                    // trim whitespace from front of line and then return it
+                    line.erase(line.begin(), std::find_if(line.begin(), line.end(), [](int ch) {
+                                   return !std::isspace(ch);
+                               }));
                     return line;
                 }
             }
@@ -39,7 +63,7 @@ namespace Specs::ExceptionHandlers {
                 std::rethrow_exception(exception);
             } catch (const snowhouse::AssertionException& e) {
                 testResult.SetFailureMessage(
-                    string_format("[{}:{}] {}\n{}", e.file(), e.line(), e.what(), ReadLineFromFile(e.file(), e.line()))
+                    string_format("\n{}{}\n[{}:{}]", e.what(), ReadLineFromFile(e.file(), e.line()), e.file(), e.line())
                 );
                 return true;
             } catch (...) {
