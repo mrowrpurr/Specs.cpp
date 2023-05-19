d3afc8152c25508f4a172e7ba19e16e52419efd7
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-18 19:26:23 -0700

    [snowhouse] Print out the failed assertion LOC

--- Specs/include/Specs/ExceptionHandlers/SnowhouseExceptionHandler.h
+++ Specs/include/Specs/ExceptionHandlers/SnowhouseExceptionHandler.h
@@ -4,6 +4,9 @@
 #include <string_format.h>
 
 #include <exception>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
 
 #include "../ISpecExceptionHandler.h"
 #include "../SpecTestCaseResult.h"
@@ -12,11 +15,32 @@ namespace Specs::ExceptionHandlers {
 
     //! Exception handler for assertions provided by the `Snowhouse` library.
     class SnowhouseExceptionHandler : public ISpecExceptionHandler {
+        std::string ReadLineFromFile(const std::string& filename, unsigned int linenumber) {
+            if (!std::filesystem::exists(filename)) return "";
+
+            std::ifstream file(filename);
+            if (!file.is_open()) return "";
+
+            std::string  line;
+            unsigned int currentLineNumber = 0;
+            while (std::getline(file, line)) {
+                currentLineNumber++;
+                if (currentLineNumber == linenumber) {
+                    return line;
+                }
+            }
+
+            return "";
+        }
+
+    public:
         bool HandleException(std::exception_ptr exception, SpecTestCaseResult& testResult) override {
             try {
                 std::rethrow_exception(exception);
             } catch (const snowhouse::AssertionException& e) {
-                testResult.SetFailureMessage(string_format("[{}:{}] {}", e.file(), e.line(), e.what()));
+                testResult.SetFailureMessage(
+                    string_format("[{}:{}] {}\n{}", e.file(), e.line(), e.what(), ReadLineFromFile(e.file(), e.line()))
+                );
                 return true;
             } catch (...) {
                 return false;
