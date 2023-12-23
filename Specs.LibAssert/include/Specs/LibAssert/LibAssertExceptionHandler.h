68a019e3132f33c7eccd3ebb329764bb24fc4513
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-22 20:02:10 -0800

    LibAssert failure support in release mode

--- Specs.LibAssert/include/Specs/LibAssert/LibAssertExceptionHandler.h
+++ Specs.LibAssert/include/Specs/LibAssert/LibAssertExceptionHandler.h
@@ -2,8 +2,19 @@
 
 #include <assert.hpp>
 
+#ifdef NDEGUG
+#include <string>
+#include <vector>
+#include <mutex>
+#endif
+
 namespace SpecsCpp::LibAssert {
     inline bool ShowStackTrace = false;
+
+    #ifdef NDEBUG
+    inline std::vector<std::string> AllAssertionFailureMessages;
+    inline std::mutex AllAssertionFailureMessagesMutex;
+    #endif
 }
 
 inline void libassert_specs_handler(
@@ -14,5 +25,13 @@ inline void libassert_specs_handler(
         std::string::size_type stack_trace_start = message.find("\nStack trace:");
         if (stack_trace_start != std::string::npos) message.erase(stack_trace_start);
     }
-    throw message.c_str();
+    #ifdef NDEBUG
+        // In release mode, the c_str() is lost unless we hold onto the string somewhere.
+        // Here we just add it to a never-ending vector. Which is fine, whatever.
+        std::lock_guard<std::mutex> lock(SpecsCpp::LibAssert::AllAssertionFailureMessagesMutex);
+        SpecsCpp::LibAssert::AllAssertionFailureMessages.emplace_back(message);
+        throw SpecsCpp::LibAssert::AllAssertionFailureMessages.back().c_str();
+    #else
+        throw message.c_str();
+    #endif
 }
