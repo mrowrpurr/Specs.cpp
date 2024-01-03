af2301618dfa90287f746c9aa5a0c98f41fbc4ee
Mrowr Purr <mrowr.purr@gmail.com>
2024-01-02 17:38:01 -0800

    Docs updates

--- docs/_specs_8_main_2include_2_specs_2_main_8h.js
+++
@@ -1,4 +0,0 @@
-var _specs_8_main_2include_2_specs_2_main_8h =
-[
-    [ "main", "_specs_8_main_2include_2_specs_2_main_8h.html#a3c04138a5bfe5d72780bb7e82a18e627", null ]
-];
--- docs/search/variables_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['list_5ftest_5fnames_5foption_5fkey_0',['LIST_TEST_NAMES_OPTION_KEY',['../struct_specs_cpp_1_1_i_spec_runner.html#af2e66ecd795938c2af90b4c78aec22ae',1,'SpecsCpp::ISpecRunner']]]
-];
